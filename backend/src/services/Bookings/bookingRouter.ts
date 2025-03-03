e097d37d3fbb3fd3badf00c12c550323012d01d3
George <George.saba2003@gmail.com>
2025-03-03 21:54:09 +0100

    Fixade logging på Bookings Mikro service

--- backend/src/services/Bookings/bookingRouter.ts
+++ backend/src/services/Bookings/bookingRouter.ts
@@ -2,10 +2,11 @@ import { deleteBooking, createBooking, getBookingForUser } from "../../controlle
 import { authenticateJWT } from "../../controllers/auth";
 import express from 'express';
 import { logging } from "../../logging";
+import { error } from "console";
 const bookingRouter = express.Router();
 // Route to create a booking with JWT authentication
 bookingRouter.post("/", authenticateJWT, async function(req, res){
-    logging("Authenticating user"); 
+    logging(`Authenticating user: ${req.user}`); 
     const hotelID = req.body.hotelID;
     const user = req.user;
     const from_date = req.body.from_date;
@@ -13,28 +14,30 @@ bookingRouter.post("/", authenticateJWT, async function(req, res){
     console.log(hotelID, user, from_date, to_date); 
     try {
         const bookingDone = await createBooking(hotelID, user, from_date, to_date);
-        logging("Auth successful!"); 
+        logging(`Booking in hotel: ${hotelID} for user: ${user}, was successfully made`);
         res.status(201).send("booking successful!");
     } catch (error){
-        logging("Auth unsuccessful!"); 
+        logging(`An error occurred, while trying to make a booking for user: ${user} at hotel: ${hotelID}. Error: ${error}`); 
         res.status(400).send(error);
     }
 });
 // Route to delete a booking by ID
 bookingRouter.delete("/", async function(req, res) {
-    logging("Deleting booking"); 
+    logging(`Got a request to delete the following booking: ${req.body.bookingId}`); 
     const bookingId = req.body.bookingId;
 
     try {
         const bookingDeleted = await deleteBooking(bookingId);
+        logging(`Successefully deleted booking with ID: ${bookingId}`);
         res.status(200).send();
     } catch {
+        logging(`An error occured while trying to delete a booking with ID: ${bookingId}. Error: ${error}`);
         res.status(400).send();
     }
 });
 // Route to get bookings for the authenticated user
 bookingRouter.get("/", authenticateJWT, async function(req, res){
-  logging("Getting bookings"); 
+  logging(`Getting bookings for user: ${req.user}`); 
   const username = req.user; 
   const bookings = await getBookingForUser(username);
   console.log(bookings); 
