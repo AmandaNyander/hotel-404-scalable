a3d4edfb2056cc0a77bf170231fd1bb9961eb1a6
Calle256 <calle.ovinder7@gmail.com>
2025-03-05 19:03:11 +0100

    uppdaterat probes och deployment

--- backend/src/services/Users/server.ts
+++ backend/src/services/Users/server.ts
@@ -6,6 +6,7 @@ import session from "express-session";
 import { read } from "fs";
 
 function createServer() {
+  logging("Code updated"); 
   const app = express();
   //Ändrade från ./health till /health för att det ska tolkas som en endpoint
   app.get('/health', (req, res) => {
