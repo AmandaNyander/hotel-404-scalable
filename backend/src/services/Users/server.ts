fcec726e7a5b693ce91c82aa6fbcd9d55cb407f2
George <George.saba2003@gmail.com>
2025-03-05 16:51:46 +0100

    lite ändringar för probing

--- backend/src/services/Users/server.ts
+++ backend/src/services/Users/server.ts
@@ -3,15 +3,37 @@ import cookieParser from "cookie-parser";
 import userRouter from "./userRouter";
 import { logging } from "../../logging";
 import session from "express-session";
+import { read } from "fs";
 
 function createServer() {
   const app = express();
-
-  app.get('./health', (req, res) => {
+  //Ändrade från ./health till /health för att det ska tolkas som en endpoint
+  app.get('/health', (req, res) => {
     logging("Health check recieved");
     res.status(200).send('OK');
   })
+  //Samtal med Erik 
+  app.get('/live', (req, res) => {
+    logging("Live check recieved");
+    res.status(200).send('OK');
+  })
 
+  let ready = false; //flagg
+
+  app.get('/ready', (req,res) =>{
+    logging(`Readiness probe hit. ready =${ready}`);
+    //För att revers eng 
+    if(ready) {
+      res.status(200).send('Ready OK')
+    } else  {
+      res.status(503).send('Not Ready');
+    }
+  });
+
+
+
+
+  //Utan erik 
   app.use(express.json());
   app.use(cookieParser());
   app.use(session({
